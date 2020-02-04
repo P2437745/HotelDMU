0925df70756c6ab9aa0202a845bec1ae7d0cda85
p2437745 <p2437745@my365.dmu.ac.uk>
2020-02-04 10:02:46 +0000

    Adding testing and class for the roomtypemanagement

---
+++ RoomType Testing/RoomtypeTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RoomType_Testing
+{
+    [TestClass]
+    public class RoomtypeTest
+    {
+        [TestMethod]
+        public void TInstanceOk()
+        {
+            //create an instance of the class we want to create
+            clsRoomType ARoomType = new clsRoomType();
+            //test to see that it exists
+            Assert.IsNotNull(ARoomType);
+        }
+        
+    }
+}
