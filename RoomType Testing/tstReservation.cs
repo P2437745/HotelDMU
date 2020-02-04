60aff5d6cd2e5025cff97cfcb2f0adf4ce438914
p2445288 <p2445288@GWHXX633WIN015W>
2020-02-04 10:15:24 +0000

    Added testing and class for Reservation

---
+++ RoomType Testing/tstReservation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RoomType_Testing
+{
+    [TestClass]
+    public class tstReservation
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create 
+            clsReservation AnReservation = new clsReservation();
+            //test to see that it exists
+            Assert.IsNotNull(AnReservation);
+        }
+    }
+}
