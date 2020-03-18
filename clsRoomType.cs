2ffa1b7a824414fad35534f64ce7524926aa56a6
P2437745 <60383978+P2437745@users.noreply.github.com>
2020-03-18 15:53:47 +0000

    Add files via upload
    Roomtype classes

---
+++ clsRoomType.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace ClassLibrary
+{
+    public class clsRoomType
+    {
+        //private data member for the RoomtypeID
+        private Int32 mRoomtypeID;
+        //private data member for RoomtypeFloorNo
+        private Int32 mRoomtypeFloorNo;
+        //private data member for RoomtypeSize
+        private string mRoomtypeSize;
+        //priate data member for Roomtype
+        private string mRoomtype;
+        //private data member for DateAdded
+        private DateTime mDateTime;
+        private Boolean mActive;
+
+
+
+
+
+
+        // public bool Active { get; set; }
+
+        public bool Find(int RoomtypeID)
+        {
+            //create an instance of the data connection 
+            clsDataConnection DB = new clsDataConnection();
+            //add the parameter for the RoomtypeID to search for 
+            DB.AddParameter("@RoomtypeID", RoomtypeID);
+            DB.Execute("sproc_tblRoomtype_FilterByRoomtype");
+            //if one record is found (there should be either one or zero!)
+            if (DB.Count == 1)
+            {
+                //copy the data from the database to the private members 
+                mRoomtypeID = Convert.ToInt32(DB.DataTable.Rows[0]["RoomtypeID"]);
+                mRoomtypeFloorNo = Convert.ToInt32(DB.DataTable.Rows[0]["RoomtypeFloorNo"]);
+                mRoomtypeSize = Convert.ToString(DB.DataTable.Rows[0]["RoomtypeSize"]);
+                mRoomtype = Convert.ToString(DB.DataTable.Rows[0]["Roomtype"]);
+                mDateTime = Convert.ToDateTime(DB.DataTable.Rows[0]["DateTime"]);
+                mActive = Convert.ToBoolean(DB.DataTable.Rows[0]["Active"]);
+                //return that everything worked OK 
+                return true;
+            }
+            //if no recrod is found
+            else
+            {
+                //return fasle indicating a problem
+                return false;
+            }
+        }
+
+        public string Valid(string text1, string text2, string text3, string text4, string text)
+        {
+            return "";
+        }
+
+        // public bool Find(int RoomtypeID)
+        //{
+        //always true 
+        // return true;
+        // }
+        public int RoomtypeID
+        {
+            get
+            {
+                //return the private data 
+                return mRoomtypeID;
+            }
+            set
+            {
+                //set the value of the private data member 
+                mRoomtypeID = value;
+            }
+
+        }
+        public DateTime DateTime
+        {
+            get
+            {
+                //return the private data 
+                return mDateTime;
+            }
+            set
+            {
+                //set the private data 
+                mDateTime = value;
+            }
+        }
+        public int RoomFloorNo
+        {
+            get
+            {
+                //retrun the private data
+                return mRoomtypeFloorNo;
+            }
+            set
+            {
+                //set the private data 
+                mRoomtypeFloorNo = value;
+            }
+        }
+        public string RoomtypeSize
+        {
+            get
+            {
+                //return the private data 
+                return mRoomtypeSize;
+            }
+            set
+            {
+                //set the private data
+                mRoomtypeSize = value;
+            }
+        }
+        public string Roomtype
+        {
+            get
+            {
+                //return the private data 
+                return mRoomtype;
+            }
+            set
+            {
+                //set the private data 
+                mRoomtype = value;
+            }
+        }
+        public bool Active
+        {
+            get
+            {
+                //return the private data
+                return mActive;
+            }
+            set
+            {
+                //set the private data 
+                mActive = value;
+            }
+        }
+
+
+
+        public string Valid(string RoomtypeFloorNo, string RoomtypeSize, string Roomtype, string DateTime)
+        {
+            //create a string variable to store the error 
+            String Error = "";
+            //if the RoomtypeFloorNo is blank 
+            if (RoomtypeFloorNo.Length == 0)
+            {
+                //RECORD THE ERROR 
+                Error = Error + "The RoomtypeFloorNo may not be blank";
+            }
+            if (RoomtypeFloorNo.Length > 50)
+            {
+                //record the error 
+                Error = Error + "The room floor number must be less than 6 :";
+            }
+            if (RoomtypeSize.Length == 0)
+            {
+                //RECORD THE ERROR 
+                Error = Error + "The Room size may not be blank";
+            }
+            if (RoomtypeSize.Length < 6)
+            {
+                //record the error 
+                Error = Error + "The room size must be less than 6 :";
+            }
+            if (Roomtype.Length == 0)
+            {
+                //RECORD THE ERROR 
+                Error = Error + "The Room size may not be blank";
+            }
+            if (Roomtype.Length < 6)
+            {
+                //record the error 
+                Error = Error + "The room size must be less than 6 :";
+            }
+            
+            return Error;
+        }
+
+
+    }
+}
---
+++ clsRoomtypeCollection.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Web;
+
+namespace ClassLibrary
+{
+    public class clsRoomtypeCollection
+    {
+
+
+        //private data memeber for the list 
+        List<clsRoomType> mRoomtypeList = new List<clsRoomType>();
+        //private data member thisRoomtype 
+        clsRoomType mThisRoomtype = new clsRoomType();
+
+        public List<clsRoomType> RoomtypeList
+        {
+            get
+            {
+                //return the private data 
+                return mRoomtypeList;
+            }
+            set
+            {
+                //set the private data 
+                mRoomtypeList = value;
+            }
+        }
+        public clsRoomType ThisRoomtype
+        {
+            get
+            {
+                //return the private data
+                return mThisRoomtype;
+            }
+            set
+            {
+                //set the private data 
+                mThisRoomtype = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return mRoomtypeList.Count;
+            }
+        }
+
+        //constructor for the class
+        public clsRoomtypeCollection()
+        {
+            //object for data connection 
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure 
+            DB.Execute("sproc_tblRoomtype_SelectAll");
+            //populate the array list with the data table 
+            PopulateArray(DB);
+        }
+        public int Add()
+        {
+            //adds a new record to the data base based on the values of mThisRoomtype
+            //set the primary key value of the new record
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure
+            DB.AddParameter("@RoomFloorNo", mThisRoomtype.RoomFloorNo);
+            DB.AddParameter("@RoomtypeSize", mThisRoomtype.RoomtypeSize);
+            DB.AddParameter("@Roomtype", mThisRoomtype.Roomtype);
+            DB.AddParameter("@DateTime", mThisRoomtype.DateTime);
+            DB.AddParameter("@Active", mThisRoomtype.Active);
+            //execute the query returning the primary key value 
+            return DB.Execute("sproc_tblRoomtype_Insert");
+
+
+        }
+        public void Delete()
+        {
+            //delete the record pointed to by ThisRoomtype 
+            //connect to the database 
+            clsDataConnection DB = new clsDataConnection();
+            //set the paramters for the stored procedur 
+            DB.AddParameter("@RoomtypeID", mThisRoomtype.RoomtypeID);
+            //execute the stored procedure 
+            DB.Execute("sproc_tblRoomtype_Delete");
+        }
+        public void Update()
+        {
+            //update an existing record based on the vlaues of ThisRoomtype 
+            //connect to the database 
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure
+            DB.AddParameter("RoomtypeID", mThisRoomtype.RoomtypeID);
+            DB.AddParameter("@RoomtypeFloorNo", mThisRoomtype.RoomFloorNo);
+            DB.AddParameter("@RoomtypeSize", mThisRoomtype.RoomtypeSize);
+            DB.AddParameter("@Roomtype", mThisRoomtype.Roomtype);
+            DB.AddParameter("@DateTime", mThisRoomtype.DateTime);
+            DB.AddParameter("@Active", mThisRoomtype.Active);
+            //execute the stored procedure 
+            DB.Execute("sproc_tblRoomtype_Update");
+
+        }
+
+        public void ReportByRoomtype(string Roomtype)
+        {
+            //filters the records based on a roomtype 
+            //connect to the database 
+            clsDataConnection DB = new clsDataConnection();
+            //send the roomtype parameter to the database 
+            DB.AddParameter("@RoomtypeID", Roomtype);
+            //execute the stored procedure 
+            DB.Execute("sproc_tblRoomtype_FilterByRoomtype");
+            //Populate the array list 
+            PopulateArray(DB);
+        }
+
+        void PopulateArray(clsDataConnection DB)
+        {
+            //populates the array list based on the data table in the parameter DB
+            //var for the index 
+            //var for the index 
+            Int32 Index = 0;
+            //var to store the record count 
+            Int32 RecordCount = 0;
+            //get the cont of records 
+            RecordCount = DB.Count;
+            //clear the private array list 
+            mRoomtypeList = new List<clsRoomType>();
+            //while there are records to process
+            while (Index < RecordCount)
+            {
+                //create a blank roomtype 
+                clsRoomType AnRoomtype = new clsRoomType();
+                //read in the fileds from the current record 
+                mThisRoomtype.Active = false;// Convert.ToBoolean(DB.DataTable.Rows[Index]["Active"]);
+                mThisRoomtype.RoomtypeID = Convert.ToInt32(DB.DataTable.Rows[Index]["RoomtypeID"]);
+                mThisRoomtype.RoomFloorNo = Convert.ToInt32(DB.DataTable.Rows[Index]["RoomtypeFloorNo"]);
+                mThisRoomtype.RoomtypeSize = Convert.ToString(DB.DataTable.Rows[Index]["RoomtypeSize"]);
+                mThisRoomtype.Roomtype = Convert.ToString(DB.DataTable.Rows[Index]["Roomtype"]);
+                mThisRoomtype.DateTime = Convert.ToDateTime(DB.DataTable.Rows[Index]["DateTime"]);
+                //add the record to the private data member 
+                mRoomtypeList.Add(mThisRoomtype);
+                //point at the next record 
+                Index++;
+            }
+        }
+
+
+    }
+}
